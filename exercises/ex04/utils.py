32cdba267cf4e7194f32119032f458e308386f5b
Will Zahrt <wzahrt@unc.edu>
2021-09-27 22:12:54 -0400

    Progress on exercise 4

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,43 @@
 """List utility functions."""
 
-__author__ = "123456789"
-
+__author__ = "730490857"
 
 # TODO: Implement your functions here.
+
+
+def all(haystack: list[int], needle: int) -> bool:
+    """Return true if the needle is found in the haystack."""
+    i: int = 0
+    while i < len(haystack):
+        item: int = haystack[i]
+        if item == needle:
+            return True
+        i += 1
+
+    return False
+        
+
+def is_equal(x: list[int], y: list[int]) -> bool:
+    i: int = 0
+    if len(x) != len(y):
+        return False
+    while i < len(x):
+        if x[i] != y[i]:
+            return False
+        i += 1
+
+    return True
+
+
+def max(x: list[int]) -> int:
+    if len(x) == 0:
+        raise ValueError("max() arg is an empty list")
+    i: int = 0
+    k: int = 0
+    while i < len(x) - 1:
+        if x[i] < x[i + 1]:
+            k = x[i + 1]
+        else:
+            k = x[i]
+        i += 1
+    return k
