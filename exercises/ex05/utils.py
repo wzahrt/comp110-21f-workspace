307f002c03aed775241a34f057c6148d60e2a705
Will Zahrt <wzahrt@unc.edu>
2021-10-05 22:19:54 -0400

    Progress on exercise 5

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,53 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730490857"
 
 # Define your functions below
+
+
+def only_evens(x: list[int]) -> list[int]:
+    """Returns a list of only even numbers."""
+    i: int = 0
+    even_list: list[int] = []
+    while i < len(x):
+        if x[i] == 0:
+            i += 1
+        elif x[i] % 2 == 0:
+            even_list.append(x[i])
+            i += 1
+        elif x[i] % 2 == 1:
+            i += 1
+    return even_list
+
+
+def sub(x: list[int], a: int, b: int) -> list[int]:
+    """A list which returns the subset of a given list between the specified start index and the end index -1."""
+    xs: list[int] = []
+    if b <= 0:
+        return xs
+    if a > b:
+        return xs
+    if len(x) == 0:
+        return xs
+    if a < 0:
+        a = 0
+    if b > len(x):
+        b = len(x)
+    while a < b:
+        xs.append(x[a])
+        a += 1
+    return xs
+
+
+def concat(xs: list[int], ys: list[int]) -> list[int]:
+    """Creates a mega-list that has all of the elements of the first list followed by all of th elements of the second."""
+    zs: list[int] = []
+    i: int = 0
+    while i < len(xs):
+        zs.append(xs[i])
+        i += 1
+    i = 0
+    while i < len(ys):
+        zs.append(ys[i])
+        i += 1
+    return zs
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -2,5 +2,63 @@
 
 # TODO: Uncomment the below line when ready to write unit tests
 # from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
+
 
 __author__ = "123456789"
+
+
+def test_utils_empty() -> None:
+    assert only_evens([]) == []
+
+
+def test_utils_two_numbers() -> None:
+    assert only_evens([1, 2]) == [2]
+
+
+def test_utils_zero_included() -> None:
+    assert only_evens([0, 1, 2, 3, 5, 0, 4]) == [2, 4]
+
+
+def test_utils_multiple_evens() -> None:
+    assert only_evens([2, 2, 2, 4, 4, 4, 3, 6]) == [2, 2, 2, 4, 4, 4, 6]
+
+
+def test_utils_negative_end_index() -> None:
+    assert sub([1, 2, 3], 1, -1) == []
+
+
+def test_utils_empty_list() -> None:
+    assert sub([], -1, 3) == []
+
+
+def test_utils_list() -> None:
+    assert sub([1, 2, 3, 4], 1, 3) == [2, 3]
+
+
+def test_utils_negative_start() -> None:
+    assert sub([1, 2, 3, 4], -1, 3) == [1, 2, 3]
+
+
+def test_utils_larger_end_index() -> None:
+    assert sub([1, 2, 3, 4], 1, 5) == [2, 3, 4]
+
+
+def test_utils_random_list() -> None:
+    assert sub([3, 4, 8, 21, 5], -60, 4) == [3, 4, 8, 21]
+
+
+def test_utils_first_use() -> None:
+    assert concat([1, 2, 3], [1, 2, 3]) == [1, 2, 3, 1, 2, 3]
+
+
+def test_utils_one_empty_list() -> None:
+    assert concat([1, 2, 3], []) == [1, 2, 3]
+
+
+def test_utils_two_empty_lists() -> None:
+    assert concat([], []) == []
+
+
+def test_utils_one_big_one_small() -> None:
+    assert concat([1, 2, 3, 4, 5, 6, 7, 8, 9], [1]) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 1]
